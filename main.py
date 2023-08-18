262ba4e0086dce15e55e71a5ca2d6ffa6a924154
anshurani1 <62585963+anshurani1@users.noreply.github.com>
2023-08-18 10:31:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# # print("1"+"2")
+# a = 1
+# b = 2
+# print(a+b)
+# a_1 = 1
+# b_1 = 2
+# print("a_1"+"b_1")
+# x= "Anshu"
+# y= "Rani"
+# # print(x+y)
+# change string to integer and add 
+a = "2"
+b = "4"
+print(int(a) + int(b))
+# implicit typecasting
+a = 100.987673
+b = 25
+print(a+b)
